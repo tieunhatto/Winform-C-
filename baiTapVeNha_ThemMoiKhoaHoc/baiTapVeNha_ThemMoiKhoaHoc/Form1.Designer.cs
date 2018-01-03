1c7bc9d4208489fd2ea527c15426ce1da01df0ef
hoangcoder96 <nhattocoder@gmail.com>
2018-01-03 23:37:58 +0700

    first commit

---
+++ baiTapVeNha_ThemMoiKhoaHoc/baiTapVeNha_ThemMoiKhoaHoc/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace baiTapVeNha_ThemMoiKhoaHoc
+{
+    partial class frmThemMoiKhoaHoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnGhi = new System.Windows.Forms.Button();
+            this.txtMaKhoa = new System.Windows.Forms.TextBox();
+            this.txtTenKhoa = new System.Windows.Forms.TextBox();
+            this.dgvTableKhoaHoc = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaKhoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenKhoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTableKhoaHoc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thêm mới khóa học";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(375, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên Khóa :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(381, 83);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã khóa :";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(585, 165);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(100, 28);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnGhi
+            // 
+            this.btnGhi.Location = new System.Drawing.Point(465, 165);
+            this.btnGhi.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGhi.Name = "btnGhi";
+            this.btnGhi.Size = new System.Drawing.Size(100, 28);
+            this.btnGhi.TabIndex = 4;
+            this.btnGhi.Text = "Ghi";
+            this.btnGhi.UseVisualStyleBackColor = true;
+            this.btnGhi.Click += new System.EventHandler(this.btnGhi_Click);
+            // 
+            // txtMaKhoa
+            // 
+            this.txtMaKhoa.Location = new System.Drawing.Point(465, 77);
+            this.txtMaKhoa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMaKhoa.Name = "txtMaKhoa";
+            this.txtMaKhoa.Size = new System.Drawing.Size(219, 22);
+            this.txtMaKhoa.TabIndex = 5;
+            // 
+            // txtTenKhoa
+            // 
+            this.txtTenKhoa.Location = new System.Drawing.Point(465, 109);
+            this.txtTenKhoa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTenKhoa.Name = "txtTenKhoa";
+            this.txtTenKhoa.Size = new System.Drawing.Size(219, 22);
+            this.txtTenKhoa.TabIndex = 6;
+            // 
+            // dgvTableKhoaHoc
+            // 
+            this.dgvTableKhoaHoc.AllowUserToAddRows = false;
+            this.dgvTableKhoaHoc.AllowUserToDeleteRows = false;
+            this.dgvTableKhoaHoc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTableKhoaHoc.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.MaKhoa,
+            this.TenKhoa});
+            this.dgvTableKhoaHoc.Location = new System.Drawing.Point(0, 133);
+            this.dgvTableKhoaHoc.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvTableKhoaHoc.Name = "dgvTableKhoaHoc";
+            this.dgvTableKhoaHoc.ReadOnly = true;
+            this.dgvTableKhoaHoc.Size = new System.Drawing.Size(447, 109);
+            this.dgvTableKhoaHoc.TabIndex = 7;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // MaKhoa
+            // 
+            this.MaKhoa.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MaKhoa.DataPropertyName = "MaKhoa";
+            this.MaKhoa.HeaderText = "Mã khóa";
+            this.MaKhoa.Name = "MaKhoa";
+            this.MaKhoa.ReadOnly = true;
+            // 
+            // TenKhoa
+            // 
+            this.TenKhoa.DataPropertyName = "TenKhoa";
+            this.TenKhoa.HeaderText = "Tên Khóa";
+            this.TenKhoa.Name = "TenKhoa";
+            this.TenKhoa.ReadOnly = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(465, 201);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // frmThemMoiKhoaHoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 244);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dgvTableKhoaHoc);
+            this.Controls.Add(this.txtTenKhoa);
+            this.Controls.Add(this.txtMaKhoa);
+            this.Controls.Add(this.btnGhi);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmThemMoiKhoaHoc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thêm mới khóa học";
+            this.Load += new System.EventHandler(this.frmThemMoiKhoaHoc_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTableKhoaHoc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnGhi;
+        private System.Windows.Forms.TextBox txtMaKhoa;
+        private System.Windows.Forms.TextBox txtTenKhoa;
+        private System.Windows.Forms.DataGridView dgvTableKhoaHoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaKhoa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenKhoa;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ baiTapVeNha_ThemMoiKhoaHoc/baiTapVeNha_ThemMoiKhoaHoc/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace baiTapVeNha_ThemMoiKhoaHoc
+{
+    public partial class frmThemMoiKhoaHoc : Form
+    {
+        public frmThemMoiKhoaHoc()
+        {
+            InitializeComponent();
+        }
+        string chuoiKetNoi = @"Data Source=DESKTOP-7P62KP4;Initial Catalog=ChuoiKetNoiWinForm;Integrated Security=True";
+
+        public void load()
+        {
+            SqlConnection cnn = new SqlConnection(chuoiKetNoi);
+            try
+            {
+                cnn.Open();
+                string sql = "select * from KhoaHoc";
+                SqlDataAdapter da = new SqlDataAdapter(sql, cnn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvTableKhoaHoc.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Lỗi kết nối");
+
+            }
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        Boolean addKhoaHoc = false;
+        private void btnGhi_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection(chuoiKetNoi);
+            try
+            {
+                if (txtMaKhoa.Text != "" && txtTenKhoa.Text != "")
+                {
+                    cnn.Open();
+                    string sql = "insert into KhoaHoc values('" + txtMaKhoa.Text + "',N'" + txtTenKhoa.Text + "')";
+                    SqlCommand cmd = new SqlCommand(sql, cnn);
+                    int kq = (int)cmd.ExecuteNonQuery();
+                    if (kq>0)
+                    {
+                        MessageBox.Show("Thêm thành công!");
+                        load();
+                    }
+                    else
+                        MessageBox.Show("Thêm thất bại!");
+                        cnn.Close();
+                }
+                else
+                    MessageBox.Show("Chưa nhập đủ thông tin!");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message,"Lỗi kết nối!");
+            }
+        }
+
+        private void frmThemMoiKhoaHoc_Load(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection(chuoiKetNoi);
+            cnn.Open();
+            string sql = "select KhoaHoc.ID,MaKhoa,TenKhoa from KhoaHoc";// lấy hết dữ liệu trong bảng sinh viên
+            SqlCommand com = new SqlCommand(sql, cnn);// Bắt đàu truy vấn
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com);// chuyển dữ liệu về
+            DataTable dt = new DataTable();// tạo 1 kho ảo để lưu trũ dữ liệu
+            da.Fill(dt);// đổ dữ liệu vào kho
+            dgvTableKhoaHoc.DataSource = dt;// đổ dữ liệu vào dataGritView
+            cnn.Close(); // đóng kêt nối
+        }
+    }
+}
